89fa868dbc54f632362dddcaa0f178851e0fb536
sjnya <sjnyas@gmail.com>
2018-03-30 01:03:16 +0900

    Create library

---
+++ forceanimateappbarlayout/src/main/java/com/github/sjnyag/ForceAnimateAppBarLayout.kt
@@ -0,0 +1,36 @@
+package com.github.sjnyag
+
+import android.content.Context
+import android.support.design.widget.AppBarLayout
+import android.util.AttributeSet
+
+class ForceAnimateAppBarLayout(context: Context, attrs: AttributeSet) : AppBarLayout(context, attrs), AppBarLayout.OnOffsetChangedListener {
+
+    private var reserveExpandAnimation: Boolean = false
+
+    init {
+        addOnOffsetChangedListener(this@ForceAnimateAppBarLayout)
+    }
+
+    fun animatedExpand() {
+        reserveExpandAnimation = true
+        setExpanded(false, false)
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        when {
+            verticalOffset == 0 -> {
+                if (reserveExpandAnimation) {
+                    reserveExpandAnimation = false
+                }
+            }
+            Math.abs(verticalOffset) >= appBarLayout?.totalScrollRange ?: 0 -> {
+                if (reserveExpandAnimation) {
+                    setExpanded(true, true)
+                }
+            }
+            else -> {
+            }
+        }
+    }
+}
